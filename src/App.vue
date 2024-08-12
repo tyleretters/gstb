421c0c7e3becfbeadfac90d7e2e721971520cb0e
Tyler Etters <tyler@etters.co>
2024-08-11 18:00:30 -0700

    may chaos take the world

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <Cube />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Cube from './components/Cube.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    Cube,
+  },
+});
+</script>
---
+++ src/components/Cube.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="wrapper">
+    <div class="cube">
+      <div class="face front"></div>
+      <div class="face back"></div>
+      <div class="face left"></div>
+      <div class="face right"></div>
+      <div class="face top"></div>
+      <div class="face bottom"></div>
+    </div>
+    <a href="https://goodshitsthebest.com" class="buy">I WOULD LIKE TO BUY.</a>
+  </div>
+</template>
+<style scoped>
+.wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 0;
+}
+
+.cube {
+  margin-top: 100px;
+  width: 200px;
+  height: 200px;
+  position: relative;
+  transform-style: preserve-3d;
+  transform: rotateX(-30deg) rotateY(-30deg);
+  animation: rotateCube 10s infinite linear;
+  z-index: 50;
+}
+
+@media screen and (min-width: 768px) {
+  .cube {
+    margin-top: 400px;
+  }
+}
+
+.face {
+  position: absolute;
+  width: 200px;
+  height: 200px;
+  background-size: cover;
+  background-position: center;
+}
+
+.front  { transform: rotateY(  0deg) translateZ(100px); background-image: url('../assets/cube-face.jpg'); }
+.back   { transform: rotateY(180deg) translateZ(100px); background-image: url('../assets/cube-face.jpg'); }
+.left   { transform: rotateY(-90deg) translateZ(100px); background-image: url('../assets/cube-face.jpg'); }
+.right  { transform: rotateY( 90deg) translateZ(100px); background-image: url('../assets/cube-face.jpg'); }
+.top    { transform: rotateX( 90deg) translateZ(100px); background-image: url('../assets/cube-face.jpg'); }
+.bottom { transform: rotateX(-90deg) translateZ(100px); background-image: url('../assets/cube-face.jpg'); }
+
+@keyframes rotateCube {
+  0% { transform: rotateX(-30deg) rotateY(-30deg); }
+  100% { transform: rotateX(-30deg) rotateY(330deg); }
+}
+
+.buy {
+  text-align: center;
+  width: auto;
+  padding: 16px;
+  height: 64px;
+  display: block;
+  text-align: center;
+  line-height: 64px;
+  font-size: 24px;
+  text-transform: uppercase;
+  text-decoration: none;
+  position: absolute;
+  background: #FF0000;
+  color: #FFFF00;
+  z-index: 100;
+  &:hover {
+    background: #FFFF00;
+    color: #FF0000;
+  }
+  top: 500px;
+}
+</style>
